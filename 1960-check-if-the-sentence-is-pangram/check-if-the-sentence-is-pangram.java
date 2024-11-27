11795f741bbada39bd30826666980c51daf7f120
Biskothu <biskothuu@gmail.com>
2024-11-27 12:01:34 +0530

    Time: 4 ms (31.36%) | Memory: 41.9 MB (13.59%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,9 +1,29 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        Set set = new HashSet<>();
-        for(int i =0; i<sentence.length(); i++){
-            set.add(sentence.charAt(i));
-        }
-        return set.size() == 26;
+        // Set set = new HashSet<>();
+        // for(int i =0; i<sentence.length(); i++){
+        //     set.add(sentence.charAt(i));
+        // }
+        // return set.size() == 26;
+
+//Method 2
+
+boolean[] arr = new boolean[26];
+int index =0;
+sentence= sentence.toLowerCase();
+for(int i=0; i<sentence.length(); i++){
+    char ch = sentence.charAt(i);
+    if(ch >= 'a' && ch <= 'z'){
+        index = ch -'a';
     }
+    arr[index++] = true;
 }
+  for(int i=0;i<arr.length;i++) {
+        if(arr[i] == false) {
+            return false;
+        }
+    }
+
+    return true;
+}
+    }
